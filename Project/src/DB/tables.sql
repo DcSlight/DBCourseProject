49dde3f5ad77f43e4a50be57b0a37bc2e1455ed6
Idan Noyshul <99333543+DcSlight@users.noreply.github.com>
2024-08-11 14:55:56 +0300

    order sql

--- Project/src/DB/tables.sql
+++ Project/src/DB/tables.sql
@@ -4,7 +4,8 @@ CREATE TABLE Customer (
     full_name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(15) NOT NULL,
     address VARCHAR(255) NOT NULL,
-    country_id INT REFERENCES countries(country_id) ON DELETE RESTRICT
+    country_id INT NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(country_id) ON DELETE RESTRICT 
 );
 
 select * from customer
@@ -36,12 +37,26 @@ select * from Product
 --Countries--
 CREATE TABLE countries (
     country_id SERIAL PRIMARY KEY,
-    country VARCHAR(255) NOT NULL,
+    country VARCHAR(255) UNIQUE NOT NULL,
     vat_rate DECIMAL(5, 2) NOT NULL, 
     currency_type VARCHAR(3) NOT NULL, 
     currency_change DECIMAL(10, 2) NOT NULL 
 );
+
 select * from countries
+
+CREATE TABLE Orders (
+    order_id VARCHAR(255) UNIQUE NOT NULL,
+    amount INT NOT NULL,
+    profit DECIMAL(10, 2) NOT NULL,
+    product_serial VARCHAR(255) NOT NULL,
+	customer_id INT NOT NULL,
+	FOREIGN KEY (product_serial) REFERENCES Product(serial) ON DELETE RESTRICT ,
+	FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT
+)
+
+
+select * from orders
 
 
 
